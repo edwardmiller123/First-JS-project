e61710a6b2dd9c15dd49990f968278ba782dca66
Edward Miller <edwardmiller123@gmail.com>
2022-05-15 17:11:31 +0100

    testing git

---
+++ jobApplicationList.js
@@ -0,0 +1,57 @@
+const applications = [{
+    company: 'CouchBase',
+    recruiter: 'none',
+    date: undefined,
+    jobTitle: 'Graduate Software Developer',
+    status: undefined,
+    apprenticeship: 'no'
+},{
+    company: 'Skuuudle',
+    recruiter: 'none',
+    date: undefined,
+    jobTitle: 'Graduate Software Developer',
+    status: undefined,
+    apprenticeship: 'no'
+}]
+
+function updateApplication(obj, companyName, prop, value) {
+    let selector = 0;
+    let updatedObj = [...obj]
+    for (let n = 0; n < updatedObj.length; n++) {
+         
+            if (updatedObj[n].company == companyName) {
+                selector = 1
+                if (updatedObj[n][prop] === undefined || updatedObj[n].hasOwnProperty(prop) == false ) {
+                    updatedObj[n][prop] = value;
+                    console.log(companyName + ' application has been updated')
+                 } else {
+                    console.log(prop + ' has value ' + updatedObj[n][prop]);
+                 }
+                } 
+    }
+    if (selector == 0) {
+        let newEntry = {company: companyName,
+                        recruiter: undefined,
+                        date: undefined,
+                        jobTitle: undefined,
+                        status: undefined,
+                        apprenticeship: undefined  }
+        newEntry[prop] = value;
+        updatedObj.push(newEntry);
+        console.log('Company has been added')
+        }
+   
+    const fsLibrary  = require('fs');
+    let data = JSON.stringify(updatedObj);
+    fsLibrary.writeFile('job applications.txt', data, (error) => {
+      
+        if (error) throw err;
+    })
+    
+    return updatedObj;
+} 
+
+
+updateApplication(applications, 'Skuuudle', 'status', 'applied');
+
+

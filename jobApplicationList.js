67ec9a66ffb082e76b3cfcba6f6b5078b829f0de
edwardmiller123 <edwardomiller123@gmail.com>
2022-05-16 16:51:10 +0100

    make it look better

--- jobApplicationList.js
+++ jobApplicationList.js
@@ -1,11 +1,11 @@
+updateApplication('job applications.txt', 'new company', 'date', '16/05/22'); 
+
 /*
 Input a file to write to, a company, a property and the value of that property to be added to the list 
 Example: updateApplication('list.txt','new company', 'date', '16/05/22' )
 To remove a company: updateApplication('list.txt', 'new company', 'delete', _)
+Text file must be in form [].
 */
-
-updateApplication('job applications.txt', 'new company', 'delete', 'applied'); 
-
 
 function updateApplication(file, companyName, prop, value) {
 

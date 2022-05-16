2fc1cbc6ab7180d63a881b6370d3468d3527213a
edwardmiller123 <edwardomiller123@gmail.com>
2022-05-16 15:15:36 +0100

    added ability to delete and entry

--- jobApplicationList.js
+++ jobApplicationList.js
@@ -9,44 +9,58 @@ fslibary.readFile('job applications.txt','utf-8', function read(err, data) {
     let applications = JSON.parse(data);
 
     function updateApplication(obj, companyName, prop, value) {
-        let selector = 0;
         let updatedObj = [...obj]
-        for (let n = 0; n < updatedObj.length; n++) {
-            
-                if (updatedObj[n].company == companyName) {
-                    selector = 1
-                    if (updatedObj[n][prop] === undefined || updatedObj[n].hasOwnProperty(prop) == false ) {
-                        updatedObj[n][prop] = value;
-                        console.log(companyName + ' application has been updated')
-                    } else {
-                        console.log(prop + ' has value ' + updatedObj[n][prop]);
-                    }
-                    } 
-        }
-        if (selector == 0) {
-            let newEntry = {company: companyName,
-                            recruiter: undefined,
-                            date: undefined,
-                            jobTitle: undefined,
-                            status: undefined,
-                            apprenticeship: undefined  }
-            newEntry[prop] = value;
-            updatedObj.push(newEntry);
-            console.log('Company has been added')
+        let selector = 0
+        if (prop == 'delete') {
+            selector = 1;
+            for (let m = 0; m < updatedObj.length; m++) {
+                if (updatedObj[m].company == companyName) {
+                    updatedObj.splice(m,1);
+                    console.log(companyName + ' has been deleted')
+                }
             }
+        } else {
+            for (let n = 0; n < updatedObj.length; n++) {
+                
+                    if (updatedObj[n].company == companyName) {
+                        selector = 2;
+                        if (updatedObj[n][prop] === undefined || updatedObj[n].hasOwnProperty(prop) == false ) {
+                            updatedObj[n][prop] = value;
+                            console.log(companyName + ' application has been updated')
+                        } else {
+                            console.log(prop + ' has value ' + updatedObj[n][prop]);
+                        }
+                        } 
+            }
+            if (selector == 0) {
+                let newEntry = {company: companyName,
+                                recruiter: undefined,
+                                date: undefined,
+                                jobTitle: undefined,
+                                status: undefined,
+                                apprenticeship: undefined  }
+                newEntry[prop] = value;
+                updatedObj.push(newEntry);
+                console.log('Company has been added')
+                }
+            }
+            const fsLibrary  = require('fs');
+            let data = JSON.stringify(updatedObj);
+            fsLibrary.writeFile('job applications.txt', data, (error) => {
+            
+                if (error) throw err;
+            })
+            
+            return updatedObj;
+        } 
     
-        const fsLibrary  = require('fs');
-        let data = JSON.stringify(updatedObj);
-        fsLibrary.writeFile('job applications.txt', data, (error) => {
-        
-            if (error) throw err;
-        })
-        
-        return updatedObj;
-    } 
+/*
+Input a company, property and the value of that property to be added to the list 
+Example: updateApplication(_,'new company', 'date', '16/05/22' )
+To remove a company: updateApplication(_, 'new company', 'delete', _)
+*/
 
-
-    console.log(updateApplication(applications, 'AECOM', 'status', 'rejected'));        
+    updateApplication(applications, 'AECOM', 'delete', 'rejected');        
 
 });
 

2f3692e0e3cc7f2aac88a5ab3095293e50bb31f1
edwardmiller123 <edwardomiller123@gmail.com>
2022-05-16 14:43:34 +0100

    fixed reading from file

--- jobApplicationList.js
+++ jobApplicationList.js
@@ -1,70 +1,54 @@
-/*const applications = [{
-    company: 'CouchBase',
-    recruiter: 'none',
-    date: undefined,
-    jobTitle: 'Graduate Software Developer',
-    status: undefined,
-    apprenticeship: 'no'
-},{
-    company: 'Skuuudle',
-    recruiter: 'none',
-    date: undefined,
-    jobTitle: 'Graduate Software Developer', 
-    status: undefined,
-    apprenticeship: 'no'
-}] */
 const fslibary = require('fs')
 
-let appString = fslibary.readFile('job applications.txt','utf-8', function read(err, data) {
-                if (err) {
-                    throw err;
-                    return;
-                }
-                console.log(data);
-
-            });
-
-
-
-let applications = JSON.parse(appString);
-
-function updateApplication(obj, companyName, prop, value) {
-    let selector = 0;
-    let updatedObj = [...obj]
-    for (let n = 0; n < updatedObj.length; n++) {
-         
-            if (updatedObj[n].company == companyName) {
-                selector = 1
-                if (updatedObj[n][prop] === undefined || updatedObj[n].hasOwnProperty(prop) == false ) {
-                    updatedObj[n][prop] = value;
-                    console.log(companyName + ' application has been updated')
-                 } else {
-                    console.log(prop + ' has value ' + updatedObj[n][prop]);
-                 }
-                } 
+fslibary.readFile('job applications.txt','utf-8', function read(err, data) {
+    if (err) {
+        throw err;
+        return;
     }
-    if (selector == 0) {
-        let newEntry = {company: companyName,
-                        recruiter: undefined,
-                        date: undefined,
-                        jobTitle: undefined,
-                        status: undefined,
-                        apprenticeship: undefined  }
-        newEntry[prop] = value;
-        updatedObj.push(newEntry);
-        console.log('Company has been added')
+
+    let applications = JSON.parse(data);
+
+    function updateApplication(obj, companyName, prop, value) {
+        let selector = 0;
+        let updatedObj = [...obj]
+        for (let n = 0; n < updatedObj.length; n++) {
+            
+                if (updatedObj[n].company == companyName) {
+                    selector = 1
+                    if (updatedObj[n][prop] === undefined || updatedObj[n].hasOwnProperty(prop) == false ) {
+                        updatedObj[n][prop] = value;
+                        console.log(companyName + ' application has been updated')
+                    } else {
+                        console.log(prop + ' has value ' + updatedObj[n][prop]);
+                    }
+                    } 
         }
-   
-    const fsLibrary  = require('fs');
-    let data = JSON.stringify(updatedObj);
-    fsLibrary.writeFile('job applications.txt', data, (error) => {
-      
-        if (error) throw err;
-    })
+        if (selector == 0) {
+            let newEntry = {company: companyName,
+                            recruiter: undefined,
+                            date: undefined,
+                            jobTitle: undefined,
+                            status: undefined,
+                            apprenticeship: undefined  }
+            newEntry[prop] = value;
+            updatedObj.push(newEntry);
+            console.log('Company has been added')
+            }
     
-    return updatedObj;
-} 
+        const fsLibrary  = require('fs');
+        let data = JSON.stringify(updatedObj);
+        fsLibrary.writeFile('job applications.txt', data, (error) => {
+        
+            if (error) throw err;
+        })
+        
+        return updatedObj;
+    } 
 
 
-console.log(updateApplication(applications, 'Skuuudle', 'status', 'applied'));
+    console.log(updateApplication(applications, 'AECOM', 'status', 'rejected'));        
+
+});
+
+
 
